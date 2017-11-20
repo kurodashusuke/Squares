dc56bc1d043335efb23c3f34dc2a1714447edbb6
50616015 <50616015@50616015-pc>
2017-11-20 17:32:09 +0900

    Squares

---
+++ src/squares/Squares_main.java
@@ -0,0 +1,26 @@
+package squares;
+
+public class Squares_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x[] = {0.1,0.3,0.5,0.7,0.9,1.1,1.3};
+		double y[] = {1.3,2.4,3.2,5.3,6.7,7.5,8.6};
+		double theta[] = new double[2];
+		
+		Squares_lib slib = new Squares_lib(x,y);
+		
+	
+
+
+	for(int i = 0;i < 100;i++) {
+		theta = slib.getDx();
+		System.out.println(i+":theta[0] ="+theta[0]);
+		System.out.println(i+":theta[1] ="+theta[1]);
+		System.out.println(i+":Objective function = "+slib.getObject());
+
+}
+	
+  }
+ }
